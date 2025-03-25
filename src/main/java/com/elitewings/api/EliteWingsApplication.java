fe293a048d6fe5f5860e7ef9de568b89070a9316
Stefany <stefany.hurtado@eia.edu.co>
2025-03-24 19:41:25 -0500

    Primer commit - Subiendo el proyecto

---
+++ src/main/java/com/elitewings/api/EliteWingsApplication.java
@@ -0,0 +1,11 @@
+package com.elitewings.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteWingsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EliteWingsApplication.class, args);
+    }
+}
